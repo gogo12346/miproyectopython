e6985302a86a57a3117e24e2cf664671bad23594
gogo12346 <raulraulandre017@gmail.com>
2025-07-08 13:51:09 -0600

    primer commit

---
+++ gogo/programa de practica.py
@@ -0,0 +1,66 @@
+#primer linea de codijo 
+print("hola universo")
+#linea de codijo no fecta?
+#no lo hizo
+"""
+esto es un comentario multilineal
+que se extiende por varias lineas
+"""
+
+#22
+#esto imprime el contenido de la variable libro 
+libro = 'ferals'
+print(libro)
+
+#estas cuatro lineas son para imprimir el contenido de las variables telf y color
+telf = '406-234 2342'
+color = 'rojo'
+print(telf)
+print(color)
+
+#reto
+a = '33'
+print(a)
+
+#esta linea es un reto
+mi_auto = 'tesla'
+
+#linea para identificar el error
+miVariable = 5
+print(miVariable) #era miVariable pero marca error y no deja continuar
+#reto nombre
+nombre = 'pablo \n santana'
+print(nombre)
+
+#reto variable cambio
+y = 10
+print(y)
+y = 15
+print(y)
+
+#reto multiples variables
+a, b, c = 10, 20, 30
+print(a, b, c)  # Imprime los valores de a, b y c
+
+#reto constante
+SALUDO = "hola universo"
+print(SALUDO)
+#suma de variables
+x = 5
+y = 3.12
+resultado = x - y
+print(resultado)
+#saludo
+nombre = 'Raúl'
+edad = '14'
+print(f"Hola {nombre}, tienes {edad} años.")
+#diccionario
+my_dict = {
+    'nombre' : 'Gerardo',
+    'edad' : 15,
+    'ciudad' : 'Monterrey',
+}
+print(my_dict["ciudad"])
+#crear un set 
+conjunto = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(conjunto)
